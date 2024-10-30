b6b1d88b7ca0e3dd942477da37e5d622745c60ee
kevinbrugnera <kevinbrugnera@gmail.com>
2024-10-30 14:50:57 +0100

    Sottosezioni
    File diviso nelle sottosezioni in cui bisogna lavorare

--- asse_Oderzo_Bagnoli.py
+++ asse_Oderzo_Bagnoli.py
@@ -2,3 +2,25 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 import pandas as pd
+
+
+#-------------------------------------------------------------------------------
+#Sezione dove si crea la classe
+
+
+
+#-------------------------------------------------------------------------------
+#SottoSezione dove si implementa il metodo per leggere Nemo_6670
+
+
+
+#-------------------------------------------------------------------------------
+#SottoSezione dove si implementa i bin
+
+
+
+#-------------------------------------------------------------------------------
+#SottoSezione dove si implementa il plot
+
+
+
